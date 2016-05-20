725ee64363eca893b648aab6310dc6c55eacdade
johnbob2014 <johnbob2014@icloud.com>
2016-05-20 21:04:34 +0800

    Initial Commit

---
+++ UICollectionView-Chapter20/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView-Chapter20
+//
+//  Created by 张保国 on 16/5/20.
+//  Copyright © 2016年 ZhangBaoGuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
